75f723551d4c382e8a41f410030ee0ae39940a96
Radharani Ribas-Valongo <rad@Radharanis-MacBook-Pro.local>
2019-08-05 15:15:03 -0400

    adds WordData class and TestTwistInfo struct

--- TextTwist/TextTwistModel.swift
+++ TextTwist/TextTwistModel.swift
@@ -9,4 +9,25 @@
 import Foundation
 
 class TextTwistModel {
+    
+    struct TestTwistInfo {
+        let wordCount: Int
+        let letters: String
+        let words: [String]
+    }
+    
+    
+    class WordData {
+        static let allInfo: [TestTwistInfo] = [
+            TestTwistInfo(wordCount: 27,
+                          letters: "cdenno",
+                          words: ["conned", "coden","coned", "donne", "nonce","code","coed","cone","conn","deco","done","neon","node","none","once", "cod","con","den","doc","doe","don","end","eon","nod","ode","one"]),
+            TestTwistInfo(wordCount: 18,
+                          letters: "ehhssu",
+                          words: ["hushes", "shush", "hehs","hues","hush","shes","sues","uses", "ess","heh","hes","hue","huh","she","shh","sue","use"]),
+            TestTwistInfo(wordCount: 14,
+                          letters: "hisswy",
+                          words: ["swishy", "hissy","swish", "hiss","whys","wish","wiss","ywis", "his","shy","sis","why","wis"])
+        ]
+    }
 }
